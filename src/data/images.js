413ede9f6c9f1dd71aa93629167719a676f7ea11
Shivm98 <shiv.stn98@gmail.com>
2021-07-06 16:03:01 +0530

    Fixed gallary images width and Added random images feature

--- src/data/images.js
+++ src/data/images.js
@@ -6,4 +6,23 @@ import image5 from "../assets/images/image-5.jpg";
 import image6 from "../assets/images/image-6.jpg";
 import image7 from "../assets/images/image-7.jpg";
 
-export default [image1, image2, image3, image4, image5, image6, image7];
+export default [
+  "https://source.unsplash.com/1200x1008/?food",
+  "https://source.unsplash.com/4770x3181/?food",
+  "https://source.unsplash.com/4770x3179/?food",
+  "https://source.unsplash.com/4770x3180/?food",
+  "https://source.unsplash.com/4770x3182/?food",
+  "https://source.unsplash.com/4770x3178/?food",
+  "https://source.unsplash.com/random/4770x3180",
+  "https://source.unsplash.com/random/4770x3181",
+  "https://source.unsplash.com/random/4770x3179",
+  "https://source.unsplash.com/random/4770x3182",
+  "https://source.unsplash.com/random/5400x2206",
+  "https://source.unsplash.com/random/5400x2207",
+  //   image2,
+  //   image3,
+  //   image4,
+  //   image5,
+  //   image6,
+  //   image7,
+];
